3fb9daa5c34475760e5d5d0c2582ed267c9f8ebe
JoaonDuarte <nunes3nunes@gmail.com>
2025-02-10 11:08:41 -0300

    gin insert

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Home(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "João Duarte",
+	})
+}
 
 func main() {
-
 	fmt.Println("Hello")
-
+	r := gin.Default()
+	r.GET("/home", Home)
+	r.Run(":8001")
+	// log.Fatal(http.ListenAndServe(":8001", r))
 }

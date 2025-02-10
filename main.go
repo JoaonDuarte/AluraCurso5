7436463eb1d46272e54d3f85dbe927e386507355
JoaonDuarte <nunes3nunes@gmail.com>
2025-02-10 11:21:47 -0300

    modulando o codigo

--- main.go
+++ main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
+	"curso5/routes"
 	"fmt"
-
-	"github.com/gin-gonic/gin"
 )
-
-func Home(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "João Duarte",
-	})
-}
 
 func main() {
 	fmt.Println("Hello")
-	r := gin.Default()
-	r.GET("/home", Home)
-	r.Run(":8001")
-	// log.Fatal(http.ListenAndServe(":8001", r))
+	routes.HandleRequest()
 }
---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"curso5/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandleRequest() {
+	r := gin.Default()
+	r.GET("/home", controllers.Home)
+	r.Run(":8001")
+	// log.Fatal(http.ListenAndServe(":8001", r))
+}
